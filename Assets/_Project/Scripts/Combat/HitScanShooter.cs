322959da9c3c46a41cef6562f7a91f4c7aa6e410
bubaleh1337 <ekaterina.pyshkova@gmail.com>
2026-04-07 15:10:08 +0500

    Add ammo and timed reload to hitscan weapon

--- Assets/_Project/Scripts/Combat/HitScanShooter.cs
+++ Assets/_Project/Scripts/Combat/HitScanShooter.cs
@@ -10,22 +10,43 @@ namespace RetroSlice.Combat
 
         [Header("Tuning")]
         [SerializeField] private float _range = 50f;
-        [SerializeField] private float _fireCooldown = 0.1f;
+        [SerializeField] private float _fireCooldown = 0.1f; // how long we must wait between shots (seconds)
+        [SerializeField] private int _maxAmmo = 12;
+        [SerializeField] private float _reloadDuration = 1.0f;
 
+        [Header("Running")]
         private float _nextFireTime;
+        private int _currentAmmo;
+        private bool _isReloading;
+        private float _reloadEndTime;
 
         void Awake()
         {
-
+            _currentAmmo = _maxAmmo;
         }
 
         void Update()
         {
-            if (Input.GetMouseButton(0) && Time.time >= _nextFireTime)
+            if (_isReloading && Time.time >= _reloadEndTime)
             {
-                _nextFireTime = Time.time + _fireCooldown;
+                _isReloading = false;
+                _currentAmmo = _maxAmmo;
+            }
+            if (Input.GetKeyDown(KeyCode.R) && !_isReloading && _currentAmmo < _maxAmmo)
+            {
+                _isReloading = true;
+                _reloadEndTime = Time.time + _reloadDuration; // reload will be finished at this absolute time
+                Debug.Log($"On reload start: Time.time = {Time.time}, _reloadlEndTime = {_reloadEndTime}");
+                return;
+            }
+            if (!_isReloading && _currentAmmo > 0 && Input.GetMouseButton(0) && Time.time >= _nextFireTime)
+            {
+                _nextFireTime = Time.time + _fireCooldown; // the earliest time when shooting is allowed again
+                Debug.LogFormat($"On fire: Time.time = {Time.time}, _fireCooldown = {_fireCooldown}");
+                _currentAmmo--;
                 Fire();
             }
+            
         }
         void Fire()
         {
@@ -36,10 +57,6 @@ namespace RetroSlice.Combat
             if (Physics.Raycast(origin, direction, out RaycastHit hit,_range))
             {
                 end = hit.point;
-            }
-            else
-            {
-                // I don't figure out where to take the "end" variable
             }
             Debug.DrawLine(origin, end, Color.red, 0.1f);
         }
